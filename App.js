a3b68d8683bf6f3ca709e9f606c1cb1f166e45e0
lukasz99999 <110409416+lukasz99999@users.noreply.github.com>
2022-08-01 17:25:51 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lista rzeczy do zrobienia</h1>
+      
+     <h2>Dodaj</h2>
+     <h3>Usun</h3>
+     <h4>Edytuj</h4>
+
+    </div>
+  );
+}
+
+export default App;
